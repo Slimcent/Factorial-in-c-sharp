43bf5d22af27d81636722e711e11d45a724a65d0
Obinna <obinna.achara@outlook.com>
2022-01-22 22:45:38 +0100

    Console Factorial in C Sharp

---
+++ Factorial c sharp/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Factorial_c_sharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtnumber = new System.Windows.Forms.TextBox();
+            this.lblresult = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btncalculate = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(95, 80);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.groupBox1.Size = new System.Drawing.Size(1054, 734);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtnumber);
+            this.groupBox3.Controls.Add(this.lblresult);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Location = new System.Drawing.Point(70, 57);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.groupBox3.Size = new System.Drawing.Size(858, 336);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            // 
+            // txtnumber
+            // 
+            this.txtnumber.Location = new System.Drawing.Point(326, 74);
+            this.txtnumber.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.txtnumber.Name = "txtnumber";
+            this.txtnumber.Size = new System.Drawing.Size(238, 48);
+            this.txtnumber.TabIndex = 5;
+            // 
+            // lblresult
+            // 
+            this.lblresult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblresult.Location = new System.Drawing.Point(326, 191);
+            this.lblresult.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.lblresult.Name = "lblresult";
+            this.lblresult.Size = new System.Drawing.Size(509, 62);
+            this.lblresult.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 191);
+            this.label2.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 40);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Result";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 74);
+            this.label1.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnExit);
+            this.groupBox2.Controls.Add(this.btnClear);
+            this.groupBox2.Controls.Add(this.btncalculate);
+            this.groupBox2.Location = new System.Drawing.Point(70, 464);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.groupBox2.Size = new System.Drawing.Size(909, 208);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(621, 80);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(238, 65);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(336, 80);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(238, 65);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "C&lear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btncalculate
+            // 
+            this.btncalculate.Location = new System.Drawing.Point(51, 80);
+            this.btncalculate.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.btncalculate.Name = "btncalculate";
+            this.btncalculate.Size = new System.Drawing.Size(238, 65);
+            this.btncalculate.TabIndex = 0;
+            this.btncalculate.Text = "&Calculate";
+            this.btncalculate.UseVisualStyleBackColor = true;
+            this.btncalculate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btncalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1235, 885);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btncalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtnumber;
+        private System.Windows.Forms.Label lblresult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Factorial c sharp/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Factorial_c_sharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            long  num, factorial;
+            long  i;
+            num = Convert.ToInt64(txtnumber.Text);
+
+            factorial = 1;
+
+            for ( i = 1; i <= num; i++)
+
+            {
+                
+                factorial = factorial * i;
+
+            }
+
+            lblresult.Text = Convert.ToString(factorial);
+
+            
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtnumber.Text = "";
+            lblresult.Text = "";
+        }
+    }
+}
